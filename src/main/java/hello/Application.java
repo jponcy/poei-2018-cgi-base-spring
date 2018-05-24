b65d8d7d59cf4b7686ed04ee67eb2e73856f368b
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2018-05-24 11:48:23 +0200

    User -"fixtures"

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,30 @@
+
 package hello;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import hello.entity.User;
+import hello.repository.UserRepository;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.userRepository.save(Arrays.asList(new User("isod"), new User("theos"), new User("harkline")));
+        this.userRepository.flush();
+    }
+
 }
